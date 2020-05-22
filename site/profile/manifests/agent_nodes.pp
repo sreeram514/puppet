05ea55db3a110283314683143df0c098c1d8a179
sreeram514 <sreeramg514@yahoo.com>
2020-05-23 00:19:37 +0530

    Create agent_nodes.pp

---
+++ site/profile/manifests/agent_nodes.pp
@@ -0,0 +1,10 @@
+class profile::agent_nodes{
+  include docker
+  docker::image { 'centos':
+    image_tage => '7'
+  }
+  docker::run {'http-server':
+    image => 'centos:7',
+    command => '/bin/sh -c "while true; do sleep 1000; done"', 
+   }
+}
